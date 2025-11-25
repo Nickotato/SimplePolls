09fb658b74791bde0f6857944c92bcb705a2995b
RealNickPotato <nickflash2654@gmail.com>
2025-11-24 20:39:16 -0600

    Initial commit

---
+++ src/main/kotlin/me/nickotato/simplePolls/SimplePolls.kt
@@ -0,0 +1,14 @@
+package me.nickotato.simplePolls
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimplePolls : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
